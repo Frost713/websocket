a0e1c26032b2c29074586570770e5f1850f6fcb9
Frost <425151654@qq.com>
2022-01-19 09:47:24 +0800

    change

--- src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -1,7 +1,4 @@
-package com.example.websocket.config;/**
- * @author Frost
- * @date 2021/11/09 10:16
- */
+package com.example.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
